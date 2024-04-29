c4098e8cc036cd28e4c9f77f2863dd65485ccb33
JBKIM <drv98@naver.com>
2024-04-29 14:28:06 +0900

    04월29일 수업

--- src/test/java/org/zerock/springex/sample/SampleTests.java
+++ src/test/java/org/zerock/springex/sample/SampleTests.java
@@ -9,6 +9,8 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 @Log4j2
 @ExtendWith(SpringExtension.class)
@@ -26,5 +28,15 @@ public class SampleTests {
     public void testService1() {
         log.info(sampleService);
         Assertions.assertNotNull(sampleService); //객체가 널이 아닐때 합격
+    }
+
+    @Test
+    public void testConnection() throws Exception {
+        //데이터소스(커넥션풀)을 이용한 DB 연결 테스트
+        Connection connection = dataSource.getConnection();
+        log.info(connection);
+        Assertions.assertNotNull(connection);
+
+        connection.close();
     }
 }
