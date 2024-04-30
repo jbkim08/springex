890557d080c195877bc70e4fa2a7ecf100461814
JBKIM <drv98@naver.com>
2024-04-30 10:14:13 +0900

    04월30일 MVC 컨트롤러

---
+++ src/main/java/org/zerock/springex/controller/TodoController.java
@@ -0,0 +1,25 @@
+package org.zerock.springex.controller;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@Log4j2
+@RequestMapping("/todo")
+public class TodoController {
+    //localhost:8080/todo/list
+    @RequestMapping("/list")
+    public void list() {
+        log.info("list");
+    }
+    //localhost:8080/todo/register (get)
+    @GetMapping("/register")
+    public void registerGet() {
+        log.info("register get method");
+    }
+    @PostMapping("/register")
+    public void registerPost() {
+        log.info("register post method");
+    }
+}
