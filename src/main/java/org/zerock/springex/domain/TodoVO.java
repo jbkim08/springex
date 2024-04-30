ae685e5234dac92bf05c4d57044715f29bc9eab1
JBKIM <drv98@naver.com>
2024-04-30 16:32:19 +0900

    04월30일 VO작성

---
+++ src/main/java/org/zerock/springex/domain/TodoVO.java
@@ -0,0 +1,18 @@
+package org.zerock.springex.domain;
+
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Getter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class TodoVO {
+    private Long tno;           //번호
+    private String title;       //제목
+    private LocalDate dueDate;  //날짜
+    private boolean finished;   //완료됨?
+    private String writer;      //작성자
+}
