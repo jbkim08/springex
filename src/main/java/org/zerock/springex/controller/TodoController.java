932667e2cb4a68cf6b7022a99700083873096468
JBKIM <drv98@naver.com>
2024-05-01 11:07:49 +0900

    5월 1일 새 할일 등록 완료

--- src/main/java/org/zerock/springex/controller/TodoController.java
+++ src/main/java/org/zerock/springex/controller/TodoController.java
@@ -1,15 +1,24 @@
 package org.zerock.springex.controller;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.zerock.springex.dto.TodoDTO;
+import org.zerock.springex.service.TodoService;
+
+import javax.validation.Valid;
 
 @Controller
 @Log4j2
 @RequestMapping("/todo")
+@RequiredArgsConstructor
 public class TodoController {
+
+    private final TodoService todoService;
+
     //localhost:8080/todo/list
     @RequestMapping("/list")
     public void list() {
@@ -22,9 +31,19 @@ public class TodoController {
     }
     //유저가 할일 입력하고 submit 버튼을 누르면 /todo/register post
     @PostMapping("/register")
-    public String registerPost(TodoDTO todoDTO, RedirectAttributes redirectAttributes) {
+    public String registerPost(@Valid TodoDTO todoDTO,
+                               BindingResult bindingResult,
+                               RedirectAttributes redirectAttributes) {
         log.info("register post method");
+
+        if (bindingResult.hasErrors()) {
+            log.info("유효성 검증 에러발생!");
+            return "redirect:/todo/register"; //검증실패시 다시 register 로 돌아감.
+        }
+        // 검증 성공 DB에 새 할일 저장
         log.info("todoDTO: " + todoDTO);
+        todoService.register(todoDTO);
+
         return "redirect:/todo/list";
     }
 }
--- src/main/java/org/zerock/springex/dto/TodoDTO.java
+++ src/main/java/org/zerock/springex/dto/TodoDTO.java
@@ -2,6 +2,8 @@ package org.zerock.springex.dto;
 
 import lombok.*;
 
+import javax.validation.constraints.Future;
+import javax.validation.constraints.NotEmpty;
 import java.time.LocalDate;
 
 //롬복으로 toString 메소드, 겟셋메소드, 빌더, 전체변수생성자, 기본생성자
@@ -12,8 +14,12 @@ import java.time.LocalDate;
 @NoArgsConstructor
 public class TodoDTO {
     private Long tno;           //번호
+    //유효성 검증으로 검증할 어노테이션을 변수에 붙임
+    @NotEmpty
     private String title;       //제목
+    @Future
     private LocalDate dueDate;  //날짜
     private boolean finished;   //완료됨?
+    @NotEmpty
     private String writer;      //작성자
 }
