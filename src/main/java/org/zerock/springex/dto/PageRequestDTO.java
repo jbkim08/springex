187401f52745e4c9a6aa25c6aaa15e82fd02aff4
JBKIM <drv98@naver.com>
2024-05-02 15:30:24 +0900

    5월 2일 페이지 번호 유지

--- src/main/java/org/zerock/springex/dto/PageRequestDTO.java
+++ src/main/java/org/zerock/springex/dto/PageRequestDTO.java
@@ -27,8 +27,21 @@ public class PageRequestDTO {
     @Max(value = 100)
     private int size = 10;
 
+    private String link; //추가링크
+
     //넘어갈 게시글 숫자
     public int getSkip(){
         return (page - 1) * size;
+    }
+
+    //현재 페이지와 사이즈를 문자열 파라미터로 리턴
+    public String getLink() {
+        if(link == null){
+            StringBuilder builder = new StringBuilder();
+            builder.append("page=" + this.page);
+            builder.append("&size=" + this.size);
+            link = builder.toString();
+        }
+        return link;
     }
 }
